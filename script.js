0118d4d21b68b658b3855607c405ee83ca3fc48c
maulanaaizm <maulanaaizm4@gmail.com>
2023-09-04 21:51:09 +0700

    menambahkan musik

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const rootElement = document.querySelector(":root");
+const audioIconWrapper = document.querySelector(".audio-icon-wrapper");
+const song = document.querySelector("#song");
+const audioIcon = document.querySelector(".audio-icon-wrapper i");
+let isPlaying = false;
+
 function disableScroll() {
-  scrollTop = window.pageXOffset || document.documentElement.scrollTop;
-  scrollLeft = window.pageYOffset || document.documentElement.scrollLeft;
+  scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
 
   window.onscroll = function () {
-    window.scrollTo(screenTop, screenLeft);
+    window.scrollTo(scrollTop, scrollLeft);
   };
 
   rootElement.style.scrollBehavior = "auto";
@@ -13,9 +18,43 @@ function disableScroll() {
 function enableScroll() {
   window.onscroll = function () {};
   rootElement.style.scrollBehavior = "smooth";
-  localStorage.setItem("opened", "true");
+  // localStorage.setItem("opened", "true");
+
+  playAudio();
 }
 
-if (!localStorage.getItem("opened")) {
-  disableScroll();
+function playAudio() {
+  // song.volume = 0.1;
+  audioIconWrapper.style.display = "flex";
+  song.play();
+  isPlaying = true;
 }
+
+audioIconWrapper.onclick = function () {
+  if (isPlaying) {
+    song.pause();
+    audioIcon.classList.remove("bi-disc");
+    audioIcon.classList.add("bi-pause-circle");
+  } else {
+    song.play();
+    audioIcon.classList.add("bi-disc");
+    audioIcon.classList.remove("bi-pause-circle");
+  }
+
+  isPlaying = !isPlaying;
+};
+
+// if (!localStorage.getItem("opened")) {
+//   disableScroll();
+// }
+disableScroll();
+
+// const urlParams = new URLSearchParams(window.location.search);
+// const nama = urlParams.get("n") || "";
+// const pronoun = urlParams.get("p") || "Bapak/Ibu/Saudara/i";
+// const namaContainer = document.querySelector(".hero h4 span");
+// namaContainer.innerText = nama;
+
+// namaContainer.innerText = ` ${pronoun} ${nama},`.replace(/ ,$/, ",");
+
+// document.querySelector("#nama").value = nama;
